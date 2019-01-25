7fa11f98708cf2967110350249a2b5f690631b9b
equablejarl <alexjohngregory@gmail.com>
2019-01-25 00:57:33 +0000

    Python work from this evening

--- superDuperCode.py
+++ superDuperCode.py
@@ -1,1 +1,37 @@
+#checking
 print 'Standard Hello World'
+
+x = 7
+y = 7
+spam = 'eggs'
+print x
+
+print spam * y
+
+print y + x
+
+print y % x
+
+print str(y) + str(x)
+
+ 
+print 'What is your name? '
+
+x = raw_input()
+
+print 'Hello, ' + x + ' How many time do you want to times your name by?'
+foo = raw_input()
+
+try: 
+	foo = int(foo)
+	print x * foo
+except ValueError:
+	print 'I cant times a name by an int!!'
+ 
+
+		
+	 	
+
+
+
+
